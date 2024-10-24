5194694b88b08c6a855178bf192fce5d25108bea
Onur45500 <onur.akmese@viacesi.fr>
2024-10-24 23:13:16 +0200

    Initialized the project

---
+++ csharp-net-wpf-racing-game/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace csharp_net_wpf_racing_game
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
